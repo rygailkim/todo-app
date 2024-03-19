37a00bd59749dbd2ec6d3c200ca1d9c849b5525d
unknown <rygailkim@gmail.com>
2024-03-19 17:05:17 +0800

    CRUD tasks

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from 'express'
 import connectToDatabase from './db'
 import userRoutes from './routes/user.routes'
 import categoryRoutes from './routes/category.routes'
+import taskRoutes from './routes/task.routes'
 
 const application = express()
 
@@ -17,6 +18,7 @@ application.get("/ping", (request: Request, response: Response) => {
 
 application.use("/users", userRoutes)
 application.use("/categories", categoryRoutes)
+application.use("/tasks", taskRoutes)
 
 application.listen(PORT, ()=> {
     console.log("Server up and running")

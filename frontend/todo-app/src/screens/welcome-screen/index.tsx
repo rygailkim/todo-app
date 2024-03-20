96a2b81a0500fda35052d8ed40f8870b9dc8324a
unknown <rygailkim@gmail.com>
2024-03-20 12:03:38 +0800

    welcome screen UI

--- frontend/todo-app/src/screens/welcome-screen/index.tsx
+++ frontend/todo-app/src/screens/welcome-screen/index.tsx
@@ -1,31 +1,69 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Box } from '@/components/utils/theme'
-import { useNavigation } from '@react-navigation/native'
-import { AuthScreenNavigationType } from '@/navigation/types'
-import Button from '@/components/shared/button'
-import SafeAreaWrapper from '@/components/shared/safe-area-wrapper'
+import Button from "@/components/shared/button"
+import SafeAreaWrapper from "@/components/shared/safe-area-wrapper"
+import { Box, Text } from "@/components/utils/theme"
+import { AuthScreenNavigationType } from "@/navigation/types"
+import { useNavigation } from "@react-navigation/native"
+import { LinearGradient } from "expo-linear-gradient"
+import React from "react"
+import { Image } from "react-native"
+import Animated, { ZoomIn } from "react-native-reanimated"
+
+const BLOSSOM_IMAGE =
+  "https://res.cloudinary.com/dooxt2sgsdooxt2sgs23233/image/upload/v1676809769/youtube/2023/february/blossom/icon_fb36u3.png"
 
 const WelcomeScreen = () => {
-    const navigation = useNavigation<AuthScreenNavigationType<"Welcome">>()
-    const navigateToSignInScreen = () => {
-        navigation.navigate("SignIn")
-    }
-    const navigateToSignUpScreen = () => {
-        navigation.navigate("SignUp")
-    }
+  const navigation = useNavigation<AuthScreenNavigationType<"Welcome">>()
+  const navigateToSignInScreen = () => {
+    navigation.navigate("SignIn")
+  }
+  const navigateToSignUpScreen = () => {
+    navigation.navigate("SignUp")
+  }
 
   return (
     <SafeAreaWrapper>
-      <Box>
-          <Text>Welcome Screen</Text>
-          <Button label="Navigate to Sign In" onPress={navigateToSignInScreen}/>
-          <Button label="Navigate to Sign Up" onPress={navigateToSignUpScreen}/>
-    </Box>
-   </SafeAreaWrapper>
+      <LinearGradient
+        colors={[
+          "#ffffff",
+          "#fcecff",
+          "#f8daff",
+          "#fae2ff",
+          "#fae2ff",
+          "#ffffff",
+        ]}
+        style={{ flex: 1 }}
+      >
+        <Box flex={1} justifyContent="center">
+          <Box alignItems="center" mb="3.5">
+              <Image
+                source={{
+                  uri: BLOSSOM_IMAGE,
+                  width: 120,
+                  height: 120,
+                }}
+              />
+          </Box>
+          <Text textAlign="center" variant="textXl" fontWeight="700">
+            Do you want to be more productive?
+          </Text>
+          <Box my="3.5" mx="10">
+            <Button
+              label="Start your journey"
+              onPress={navigateToSignUpScreen}
+            />
+          </Box>
+          <Text
+            textAlign="center"
+            variant="textXs"
+            fontWeight="700"
+            color="gray5"
+          >
+            26,698 registered today
+          </Text>
+        </Box>
+      </LinearGradient>
+    </SafeAreaWrapper>
   )
 }
 
 export default WelcomeScreen
-
-const styles = StyleSheet.create({})

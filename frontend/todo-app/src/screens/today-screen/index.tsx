ae0754702e33ab2d32a8295903d84da54b1bd825
unknown <rygailkim@gmail.com>
2024-03-21 12:23:53 +0800

    today screen

--- frontend/todo-app/src/screens/today-screen/index.tsx
+++ frontend/todo-app/src/screens/today-screen/index.tsx
@@ -1,13 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import SafeAreaWrapper from '@/components/shared/safe-area-wrapper'
+import Loader from "@/components/shared/loader"
+import NavigateBack from "@/components/shared/navigate-back"
+import SafeAreaWrapper from "@/components/shared/safe-area-wrapper"
+import Task from "@/components/tasks/task"
+import { fetcher } from "@/services/config"
+import { ITask } from "@/types"
+import { Box, Text } from "@/utils/theme"
+import React from "react"
+import { FlatList } from "react-native"
+import useSWR from "swr"
 
 const TodayScreen = () => {
+  const {
+    data: tasks,
+    isLoading: isLoadingTasks,
+    mutate: mutateTasks,
+  } = useSWR<ITask[]>(`tasks/today`, fetcher)
+
+  if (isLoadingTasks || !tasks) {
+    return <Loader />
+  }
+
   return (
     <SafeAreaWrapper>
-      <View>
-        <Text>TodayScreen</Text>
-      </View>
+      <Box flex={1} mx="4">
+        <Box height={16} />
+        <Box flexDirection="row">
+          <Text variant="textXl" fontWeight="700" ml="3">
+            Today
+          </Text>
+        </Box>
+        <Box height={16} />
+
+        <FlatList
+          data={tasks}
+          renderItem={({ item, index }) => {
+            return <Task task={item}  />
+          }}
+          ItemSeparatorComponent={() => <Box height={14} />}
+          keyExtractor={(item) => item._id}
+        />
+      </Box>
     </SafeAreaWrapper>
   )
 }
